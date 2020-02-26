7da162f644301357bb5b865d01ade627370790bc
Eugene Popovich <budik.work@gmail.com>
2020-02-26 20:51:08 +0300

    Hid admin and password variables

--- outputs.tf
+++ outputs.tf
@@ -2,13 +2,13 @@ output "url" {
   value = "https://${google_container_cluster.gke_cluster.endpoint}"
 }
 
-output "admin_username" {
-  value = "${var.master_username}"
-}
+// output "admin_username" {
+//   value = "${var.master_username}"
+// }
 
-output "admin_password" {
-  value = "${var.master_password}"
-}
+// output "admin_password" {
+//   value = "${var.master_password}"
+// }
 
 output "client_certificate" {
   value = "${base64decode(google_container_cluster.gke_cluster.master_auth.0.client_certificate)}"
